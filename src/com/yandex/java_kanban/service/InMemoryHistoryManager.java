eb152da3cffac8a1df5adf8fd4e4baa57538604d
Tilibom1983 <ygra@list.ru>
2024-10-20 03:43:56 +0300

    еще поправка

--- src/com/yandex/java_kanban/service/InMemoryHistoryManager.java
+++ src/com/yandex/java_kanban/service/InMemoryHistoryManager.java
@@ -5,17 +5,20 @@ import com.yandex.java_kanban.model.Task;
 import java.util.LinkedList;
 import java.util.List;
 
-public class InMemoryHistoryManager implements HistoryManager{
+public class InMemoryHistoryManager implements HistoryManager {
     private static final int historySize = 10;
     private final List<Task> history = new LinkedList<>();
+
     @Override
     public void addToHistory(Task task) {
-        if(task != null) {
-            if (history.size() == historySize) {
-                history.removeFirst();
-            }
-            history.add(task);
+        if (task == null) {
+            System.out.println("Задача не найдена.");
         }
+        if (history.size() == historySize) {
+            history.removeFirst();
+        }
+        history.add(task);
+
     }
 
     @Override
