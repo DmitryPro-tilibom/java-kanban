212d0400199dba6fb9ce51576c915247f4b154ba
Dmitry <Ardenna110@yandex.ru>
2024-12-05 17:37:53 +0300

    Update Main.java

--- src/com/yandex/java_kanban/Main.java
+++ src/com/yandex/java_kanban/Main.java
@@ -1,13 +1,5 @@
 package com.yandex.java_kanban;
 
-import com.yandex.java_kanban.model.Epic;
-import com.yandex.java_kanban.model.Status;
-import com.yandex.java_kanban.model.SubTask;
-import com.yandex.java_kanban.model.Task;
-import com.yandex.java_kanban.service.*;
-
-import java.io.File;
-
 public class Main {
     public static void main(String[] args) {
 
