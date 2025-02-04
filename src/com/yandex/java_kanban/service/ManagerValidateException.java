6ad6ce671b6da99f04a5e3d254699215ac7cf531
Dmitry <Ardenna110@yandex.ru>
2025-02-04 19:46:16 +0300

    Update ManagerValidateException.java

--- src/com/yandex/java_kanban/service/ManagerValidateException.java
+++ src/com/yandex/java_kanban/service/ManagerValidateException.java
@@ -1,6 +1,6 @@
 package com.yandex.java_kanban.service;
 
-public class ManagerValidateException extends RuntimeException{
+public class ManagerValidateException extends RuntimeException {
     public ManagerValidateException(String message) {
         super(message);
     }
